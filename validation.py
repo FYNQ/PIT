1e1cc996e4e1d0b69335f630f4ce443e65b106c8
Markus Kreidl <mkreidl@opentech.at>
2018-05-30 10:23:54 +0530

    PIT3: validation - added list of functions to be verified
    Signed-off-by: Markus Kreidl <mkreidl@opentech.at>

---
+++ validation.py
@@ -0,0 +1,7 @@
+cus = ['include/linux/gfp.h', 'drivers/pci/setup-res.c','drivers/crypto/talitos.c' ,'drivers/mmc/core/mmc_ops.c' ,'arch/powerpc/sysdev/fsl_pci.h','kernel/events/core.c','kernel/rcu/tree.c','kernel/trace/trace.c','kernel/cgroup.c','mm/readahead.c','mm/page_alloc.c','mm/dmapool.c', 'mm/mmap.c', 'lib/vsprintf.c', 'net/netfilter/nf_tables_api.c', 'net/netfilter/nf_tables_core.c', 'net/ipv4/ip_output.c', 'net/ipv4/netfilter/arp_tables.c', 'net/ipv4/syncookies.c', 'net/ipv4/inet_hashtables.c',
+        'net/core/sock.c', 'net/packet/af_packet.c', 'fs/proc/array.c', 'fs/ext4/inode.c', 'security/keys/request_key_auth.c']
+
+
+funs = ['cgroup_disable', 'ext4_journalled_write_end', 'of_irq_init', 'show_free_areas', 'sk_clone_lock', 'arch_get_unmapped_area_topdown', 'perf_pmu_cancel_txn', 'bstr_printf', 'watchdog_set_timeout', 'gfpflags_to_migratetype', 'arpt_do_table', 'talitos_edesc_alloc', 'pci_revert_fw_address', 'print_bin_fmt', 'compute_score', 'request_key_auth_new', '__nft_trace_packet', 'packet_bind_spkt', 'ondemand_readahead', 'run_filter', 'synchronize_sched_expedited_wait','tcp_get_cookie_sock', 'synchronize_sched_expedited', 'dma_pool_alloc', 'fsl_pci_mcheck_exception', 'mmc_send_tuning', 'task_name', 'ip_do_fragment', 'ext4_writepage', 'nf_tables_newchain']
+
+
