4bc571fa29395f7689327b1b8c9638a3ae062099
Markus Kreidl <mkreidl@opentech.at>
2018-10-31 16:32:26 +0100

    PIT3: Replace tags.txt with solution written in python
    Signed-off-by: Markus Kreidl <mkreidl@opentech.at>

--- src/python/check_series.py
+++ src/python/check_series.py
@@ -61,7 +61,10 @@ def read_reqs(fname):
         req.update({l[0]: l[1:]})
     return req
 
-
+def get_tags(path):
+    _tags = aux.do_cmd('git tag --sort=v:refname', conf.LINUX_SRC, None).split('\n')
+    tags = _tags[_tags.index('v3.0'):-1]
+    return tags
 
 def read_tags(fname):
     f = open(fname)
@@ -69,6 +72,93 @@ def read_tags(fname):
     tags = [t.rstrip() for t in _tags]
     f.close()
     return tags
+
+def find_tag_item(versions, base):
+    for vers in versions:
+        if base in list(vers.keys())[0]:
+            return vers[base]
+
+def prep_tags(tags):
+    idx = tags.index('v3.2')
+    tags = tags[idx:]
+    main_vers = {}
+    for tag in tags:
+        if not 'rc' in tag:
+            if len(tag.split('.'))== 2:
+                main_vers.update({tag:[]})
+                start_tag = tag.split('.')[0]+'.' + str(int(tag.split('.')[1])-1)
+                main_vers[tag].append(start_tag)
+
+    for i, tag in enumerate(tags):
+        f = False
+        if 'rc' in tag:
+            base = tag.split('-')[0]
+            main_vers[base].append(tag)
+        if i + 1 < len(tags):
+            if 'rc' in tag and not 'rc' in tags[i+1]:
+                main_vers[base].append(base)
+
+
+
+    for tag in tags:
+        if not 'rc' in tag and len(tag.split('.')) == 3:
+            n = tag.split('.')
+            base = '.'.join(n[0:2])
+            main_vers[base].append(tag)
+
+    return main_vers
+
+def get_todo_tags(first, last, tags):
+    # e.g do tag v4.3
+    # we need v4.2, rc tags, v4.3 tag and rest
+    todo_tags = []
+
+    if len(first.split('.')) == 2 and not 'rc' in first:
+        first_is_base = True
+    else:
+        first_is_base = False
+
+    if first.split('.')[:2] == last.split('.')[:2]:
+        same_base = True
+    else:
+        same_base = False
+
+    #if first_is_base and last_is_base:
+    if not 'rc' in first:
+        if same_base == True:
+            n = first.split('.')
+            base = '.'.join(n[0:2])
+        else:
+            n = last.split('.')
+            base = '.'.join(n[0:2])
+
+    else:
+        base = first.split('-')[0]
+
+    if first_is_base and same_base:
+        t = tags[base]
+        idx = tags[base].index(first)
+        for tag in tags[base][idx:]:
+            todo_tags.append(tag)
+            if tag == last:
+                break
+    elif first_is_base:
+        for tag in tags[base]:
+            todo_tags.append(tag)
+            if tag == last:
+                break
+
+
+    if first_is_base == False and sam_base:
+        idx = tags[base].index(first)
+        for tag in tags[base][idx:]:
+            todo_tags.append(tag)
+            if tag == last:
+                break
+
+    print("Tags todo: %s" % todo_tags)
+    return todo_tags
+
 
 
 def compile_kernel(path_proj, linux_path, kconfig, arch, tag):
@@ -123,7 +213,10 @@ def do_mp(first, last, kconfig, arch):
 
 
     do_tags = []
-    tags = read_tags("tags.txt")
+#    tags = read_tags("tags.txt")
+    tags = get_tags(conf.LINUX_SRC)
+    tags_a = prep_tags(tags)
+    tags = get_todo_tags(first, last, tags_a)
     req = read_reqs('req_compiler.txt')
     append = False
     cnt = 0
