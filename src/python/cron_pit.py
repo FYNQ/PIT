9c501d47385ba6d6c47a204207099e2402e6ad85
Markus Kreidl <mkreidl@opentech.at>
2019-05-03 11:37:37 -0300

    PIT3: cron_pit, change do_cmd parameter, add main function
    Signed-off-by: Markus Kreidl <mkreidl@opentech.at>

--- src/python/cron_pit.py
+++ src/python/cron_pit.py
@@ -8,7 +8,7 @@ import write_data as wd
 linux_src = conf.LINUX_SRC
 
 exec_new_tag = True
-list_jobs = './jobs.txt'
+list_jobs = '/home/user/src/python/jobs.txt'
 
 
 
@@ -29,31 +29,26 @@ def read_tags(fname):
     return tags
 
 
-def do_cmd(cmd, path, logger):
+def do_cmd(cmd, path, show):
     process = subprocess.Popen(cmd, cwd=path, shell=True,
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     out, err = process.communicate()
     process.wait()
     res = out.decode("utf-8")
-    if logger != None:
-        if path != None:
-            logger.info("do cmd: %s in path: %s" % (cmd, path))
-        else:
-            logger.info("do cmd: %s" % (cmd))
-        logger.info(res)
-    else:
-        print("do cmd: %s in path: %s" % (cmd, path))
+    print("do cmd: %s in path: %s" % (cmd, path))
+    if show == True:
+        print(res)
 
     if process.returncode != 0:
         exit()
     return res
 
 def git_pull(path):
-    do_cmd('git pull', linux_src, None)
+    do_cmd('git pull', linux_src, True)
 
 def get_tags(path):
-    _tags = do_cmd('git tag --sort=v:refname', linux_src, None).split('\n')
+    _tags = do_cmd('git tag --sort=v:refname', linux_src, False).split('\n')
     tags = _tags[_tags.index('v3.0'):-1]
     return tags
 
@@ -84,62 +79,66 @@ def start_job(job, from_tag, to_tag, kconfig):
             (kconfig, from_tag, to_tag[0])
     print("Execute: %s" % cmd)
     cwd = os.getcwd()
-    do_cmd(cmd, cwd, None)
-
-# git pull to get new tags
-git_pull(linux_src)
-tags = get_tags(linux_src)
-
-jobs = open(list_jobs).read().split('\n')
-f = open(list_done, 'r')
-jobs_done = json.load(f)
-f.close()
+    do_cmd(cmd, cwd, False)
 
 
+def start():
+    # git pull to get new tags
+    git_pull(linux_src)
+    tags = get_tags(linux_src)
 
-for _job in jobs:
-    if len(_job) == 0 or _job.startswith("#"):
-        continue
+    jobs = open(list_jobs).read().split('\n')
+    f = open(list_done, 'r')
+    jobs_done = json.load(f)
+    f.close()
 
-    job = _job.split(' ')[0]
-    kconfig = _job.split(' ')[1]
-    arch = _job.split(' ')[2]
-    prefix = res_loc + '/' + job.replace('.','_') + '_'
-
-    if not job in jobs_done.keys():
-        todo_tags = get_todo_tag(job, tags)
-        start_tag = todo_tags[0]
-        end_tag = todo_tags[-1:][0]
-        done_lst.update({job:todo_tags})
-
-    elif job in jobs_done.keys():
-        todos = []
-        jdone = jobs_done[job]
-        cur_tags = get_todo_tag(job, tags)
-        for todo_tag in cur_tags:
-            if not todo_tag in jdone:
-                todos.append(todo_tag)
-
-        if len(todos) == 0:
-            print("jipii nothing to do!")
+    for _job in jobs:
+        if len(_job) == 0 or _job.startswith("#"):
             continue
 
-        start_tag = jdone[-1:][0]
-        end_tag = todos[-1:][0]
+        job = _job.split(' ')[0]
+        kconfig = _job.split(' ')[1]
+        arch = _job.split(' ')[2]
+        prefix = res_loc + '/' + job.replace('.','_') + '_'
 
-        with open(list_done, 'r') as f:
-             done_lst = json.load(f)
-        done_lst[job].extend(todos)
+        if not job in jobs_done.keys():
+            todo_tags = get_todo_tag(job, tags)
+            start_tag = todo_tags[0]
+            end_tag = todo_tags[-1:][0]
+            done_lst.update({job:todo_tags})
+
+        elif job in jobs_done.keys():
+            todos = []
+            jdone = jobs_done[job]
+            cur_tags = get_todo_tag(job, tags)
+            for todo_tag in cur_tags:
+                if not todo_tag in jdone:
+                    todos.append(todo_tag)
+
+            if len(todos) == 0:
+                print("jipii nothing to do!")
+                continue
+
+            start_tag = jdone[-1:][0]
+            end_tag = todos[-1:][0]
+
+            with open(list_done, 'r') as f:
+                 done_lst = json.load(f)
+            done_lst[job].extend(todos)
+            with open(list_done, 'w') as f:
+                 json.dump(done_lst, f)
+
+
+        print("First: %s  Last: %s" % (start_tag, end_tag))
+        done_tags = check_series.do_mp(start_tag, end_tag, kconfig, arch)
+
+
         with open(list_done, 'w') as f:
-             json.dump(done_lst, f)
+            json.dump(done_lst, f)
 
+        wd.wr_results(job, jobs_done, kconfig, prefix)
 
-    print("First: %s  Last: %s" % (start_tag, end_tag))
-    done_tags = check_series.do_mp(start_tag, end_tag, kconfig, arch)
+if __name__ == '__main__':
+    start()
 
-
-    with open(list_done, 'w') as f:
-        json.dump(done_lst, f)
-
-    wd.wr_results(job, jobs_done, kconfig, prefix)
 
